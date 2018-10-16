6f81c18a7c8cad57a8cbc4d545828c430d1ac91f
mgechev <mgechev@gmail.com>
2018-10-16 13:02:49 -0700

    Update node implementation

--- transfer/net.ts
+++ transfer/net.ts
@@ -35,23 +35,80 @@ const imageToInput = (image, numChannels) => {
   return input;
 };
 
-const loadModel = async path => {
+const loadModel = async (path, layer) => {
   const mn = new mobilenet.MobileNet(1, 1);
   mn.path = `file://${path}`;
   await mn.load();
-  return mn;
+  return input => mn.infer(input, layer);
 };
 
-const classify = async (model, path) => {
-  const image = readImage(path);
-  const input = imageToInput(image, NUMBER_OF_CHANNELS);
+const Layer = 'global_average_pooling2d_1';
+const Positive = 'hits-aug';
+const Negative = 'no-hits-aug';
+const Epochs = 80;
+const BatchSize = 0.2;
+const ModelPath = './model/model.json';
 
-  const mn_model = await loadModel(model);
-  const predictions = await mn_model.classify(input);
+const train = async () => {
+  const mobileNet = await loadModel(ModelPath, Layer);
+  const model = tf.sequential();
+  model.add(tf.layers.inputLayer({ inputShape: [1024] }));
+  model.add(tf.layers.dense({ units: 1024, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 1024, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+  await model.compile({
+    optimizer: tf.train.adam(0.0001),
+    loss: tf.losses.sigmoidCrossEntropy,
+    metrics: ['accuracy']
+  });
 
-  console.log('classification results:', predictions);
+  const positives = require('fs')
+    .readdirSync(Positive)
+    .filter(f => f.endsWith('.jpg'))
+    .map(f => `${Positive}/${f}`)
+    .slice(0, 1000);
+  const negatives = require('fs')
+    .readdirSync(Negative)
+    .filter(f => f.endsWith('.jpg'))
+    .map(f => `${Negative}/${f}`)
+    .slice(0, 500);
+
+  const ys = tf.tensor2d(new Array(positives.length).fill(1).concat(new Array(negatives.length).fill(0)), [
+    positives.length + negatives.length,
+    1
+  ]);
+
+  let xs = null;
+  positives.forEach((path: string) => {
+    const image = readImage(path);
+    const input = imageToInput(image, NUMBER_OF_CHANNELS);
+    const res = mobileNet(input);
+    if (xs === null) {
+      xs = res;
+    } else {
+      xs = xs.concat(res);
+    }
+  });
+
+  negatives.forEach((path: string) => {
+    const image = readImage(path);
+    const input = imageToInput(image, NUMBER_OF_CHANNELS);
+    const res = mobileNet(input);
+    xs = xs.concat(res);
+  });
+
+  await model.fit(xs, ys, {
+    epochs: Epochs,
+    batchSize: (positives.length + negatives.length) * BatchSize,
+    callbacks: {
+      onBatchEnd: async (_, logs) => {
+        console.log('Cost: %s, accuracy: %s', logs.loss.toFixed(5), logs.acc.toFixed(5));
+        await tf.nextFrame();
+      }
+    }
+  });
+
+  await model.save('file://punch_model');
 };
 
-if (process.argv.length !== 4) throw new Error('incorrect arguments: node script.js <MODEL> <IMAGE_FILE>');
-
-classify(process.argv[2], process.argv[3]);
+train();
