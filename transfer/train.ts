cfb5597d63d385b38c5bd78a2648a4475c94517c
mgechev <mgechev@gmail.com>
2018-10-18 15:27:50 -0400

    Update the network and gitignore

--- transfer/train.ts
+++ transfer/train.ts
@@ -5,17 +5,14 @@ import * as tf from '@tensorflow/tfjs';
 require('@tensorflow/tfjs-node');
 
 const Hits = 'hits-aug';
-const Kicks = 'kicks-aug';
 const Negative = 'no-hits-aug';
-const Epochs = 100;
-const BatchSize = 0.4;
+const Epochs = 50;
+const BatchSize = 0.1;
 
 const train = async () => {
   const mobileNet = await loadModel();
   const model = tf.sequential();
   model.add(tf.layers.inputLayer({ inputShape: [1024] }));
-  model.add(tf.layers.conv2d({ filters: 4, kernelSize: 800, padding: 'same' }));
-  model.add(tf.layers.avgPool1d({}));
   model.add(tf.layers.dense({ units: 1024, activation: 'relu' }));
   model.add(tf.layers.dense({ units: 2, activation: 'softmax' }));
   await model.compile({
@@ -27,25 +24,18 @@ const train = async () => {
   const hits = require('fs')
     .readdirSync(Hits)
     .filter(f => f.endsWith('.jpg'))
-    .map(f => `${Hits}/${f}`)
-    .slice(1);
-  const kicks = require('fs')
-    .readdirSync(Kicks)
-    .filter(f => f.endsWith('.jpg'))
-    .map(f => `${Kicks}/${f}`)
-    .slice(1);
+    .map(f => `${Hits}/${f}`);
+
   const negatives = require('fs')
     .readdirSync(Negative)
     .filter(f => f.endsWith('.jpg'))
-    .map(f => `${Negative}/${f}`)
-    .slice(1);
+    .map(f => `${Negative}/${f}`);
 
   const ys = tf.tensor2d(
     new Array(hits.length)
       .fill([1, 0])
-      .concat(new Array(kicks.length).fill([0, 1]))
       .concat(new Array(negatives.length).fill([0, 0])),
-    [hits.length + kicks.length + negatives.length, 2]
+    [hits.length + negatives.length, 2]
   );
 
   let xs: tf.Tensor = null;
@@ -59,12 +49,6 @@ const train = async () => {
     }
   });
 
-  kicks.forEach((path: string) => {
-    const input = readInput(path);
-    const res = mobileNet(input);
-    xs = xs.concat(res);
-  });
-
   negatives.forEach((path: string) => {
     const input = readInput(path);
     const res = mobileNet(input);
@@ -73,7 +57,7 @@ const train = async () => {
 
   await model.fit(xs, ys, {
     epochs: Epochs,
-    batchSize: parseInt(((hits.length + kicks.length + negatives.length) * BatchSize).toFixed(0)),
+    batchSize: parseInt(((hits.length + negatives.length) * BatchSize).toFixed(0)),
     callbacks: {
       onBatchEnd: async (_, logs) => {
         console.log('Cost: %s, accuracy: %s', logs.loss.toFixed(5), logs.acc.toFixed(5));
@@ -82,7 +66,8 @@ const train = async () => {
     }
   });
 
-  await model.save('file://no-aug-all');
+  await model.save('file://punch_simplified');
 };
 
 train();
+
