a0eae047ab69017b5991ebb60bf09c13859624f5
mgechev <mgechev@gmail.com>
2018-10-22 20:42:49 -0700

    Update params

--- multilayer-perceptron/index.ts
+++ multilayer-perceptron/index.ts
@@ -2,13 +2,13 @@ import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-node';
 import * as fs from 'fs';
 
-const getModel = async () => {
+const createModel = async () => {
   const model = tf.sequential();
   model.add(tf.layers.dense({ inputShape: [1], units: 7, activation: 'relu' }));
   model.add(tf.layers.dense({ units: 10, activation: 'relu' }));
   model.add(tf.layers.dense({ units: 4, activation: 'softmax' }));
   await model.compile({
-    optimizer: tf.train.adam(0.2),
+    optimizer: tf.train.adam(0.001),
     loss: tf.losses.softmaxCrossEntropy,
     metrics: ['accuracy']
   });
@@ -17,11 +17,11 @@ const getModel = async () => {
 
 const train = async (model, data) => {
   console.log('Total examples:', data.length, data.map);
-  const xs = tf.tensor2d(data.map(d => [d[0] / 365]));
+  const xs = tf.tensor1d(data.map(d => d[0] / 365));
   const ys = tf.stack(data.map(d => d[1]));
   await model.fit(xs, ys, {
     batchSize: Math.floor(0.4 * data.length),
-    epochs: 200,
+    epochs: 800,
     callbacks: {
       onEpochEnd: async (_, logs) => {
         console.log('Cost: %s, accuracy: %s', logs.loss.toFixed(5), logs.acc.toFixed(5));
@@ -39,9 +39,9 @@ const evaluate = async (model, data) => {
 };
 
 const result = shuffle(JSON.parse(fs.readFileSync('data.json').toString()));
-const training = result.slice(0, Math.floor(result.length * 0.9));
-const test = result.slice(Math.floor(result.length * 0.9), result.length);
-getModel().then(async model => {
+const training = result.slice(0, Math.floor(result.length * 0.8));
+const test = result.slice(Math.floor(result.length * 0.8), result.length);
+createModel().then(async model => {
   await train(model, training);
   console.log('Evaluating:');
   await evaluate(model, test);
